b0026df2732394b2b28ccffde8b5bae636efaf44
kiran@simpledatalabs.com <kiran@simpledatalabs.com>
2025-02-10 13:10:38 +0000

    Add new SQL macro `gem1` and SQL model `asdasd`

---
+++ projectPath/gems/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+setup(
+    name = 'ExampleExt',
+    version = '0.0.1',
+    packages = ['ExampleExt'],
+    package_dir = {'ExampleExt': '.'},
+    description = '',
+    install_requires = [],
+)
+
